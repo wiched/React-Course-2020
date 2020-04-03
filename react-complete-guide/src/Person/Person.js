7c27bc07a80c2ae10433bccc5ca70c50ad252d74
Wiched <wiched@users.noreply.github.com>
2020-04-04 00:25:13 +0300

    First commit

---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Person.css'
+const Person = (props) => {
+  return (
+    <div className="Person">
+      <p>I am {props.name} and I am {props.age} years old</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  )
+}
+
+export default Person;
